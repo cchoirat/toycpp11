3951ecf5edbd3a29554370cf0a4e8c9720f29578
cchoirat <cchoirat@gmail.com>
2018-11-16 13:31:34 -0500

    Testing C++ 11

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,28 @@
 
 using namespace Rcpp;
 
+// useCpp11
+int useCpp11();
+RcppExport SEXP _toycpp11_useCpp11() {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    rcpp_result_gen = Rcpp::wrap(useCpp11());
+    return rcpp_result_gen;
+END_RCPP
+}
+// myhypot
+double myhypot(double x, double y);
+RcppExport SEXP _toycpp11_myhypot(SEXP xSEXP, SEXP ySEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< double >::type x(xSEXP);
+    Rcpp::traits::input_parameter< double >::type y(ySEXP);
+    rcpp_result_gen = Rcpp::wrap(myhypot(x, y));
+    return rcpp_result_gen;
+END_RCPP
+}
 // rcpp_hello_world
 List rcpp_hello_world();
 RcppExport SEXP _toycpp11_rcpp_hello_world() {
@@ -17,6 +39,8 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_toycpp11_useCpp11", (DL_FUNC) &_toycpp11_useCpp11, 0},
+    {"_toycpp11_myhypot", (DL_FUNC) &_toycpp11_myhypot, 2},
     {"_toycpp11_rcpp_hello_world", (DL_FUNC) &_toycpp11_rcpp_hello_world, 0},
     {NULL, NULL, 0}
 };
--- src/rcpp_hello_world.cpp
+++ src/rcpp_hello_world.cpp
@@ -2,6 +2,20 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+// [[Rcpp::plugins(cpp11)]]
+
+// [[Rcpp::export]]
+int useCpp11() {
+  auto x = 10;
+  return x;
+}
+
+// [[Rcpp::export]]                                                                       
+double myhypot(double x, double y) {
+  // https://en.cppreference.com/w/cpp/numeric/math/hypot
+  return std::hypot(x,y);
+}
+
 // [[Rcpp::export]]
 List rcpp_hello_world() {
 
